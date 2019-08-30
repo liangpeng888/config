f07a62c04c3ca4f2c1da4b88c80a12a467d17e41
1006820364@qq.com <1006820364@qq.com>
2019-08-30 11:01:37 +0800

    name提交

--- src/main/java/com/imooc/model/Test.java
+++ src/main/java/com/imooc/model/Test.java
@@ -10,6 +10,16 @@ public class Test {
 
     private String id;
 
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getId() {
         return id;
     }
