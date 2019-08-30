65910ea7ea8332ee32f87c43e19c367a30a4b2e7
1006820364@qq.com <1006820364@qq.com>
2019-08-30 11:09:02 +0800

    修改代码

--- src/main/java/com/imooc/model/Test.java
+++ src/main/java/com/imooc/model/Test.java
@@ -7,24 +7,4 @@ package com.imooc.model;
  * @create: 2019-08-30 09:16
  **/
 public class Test {
-
-    private String id;
-
-    private String userName;
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 }
