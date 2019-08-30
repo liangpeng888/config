94fb4acd536d9db66eeeb7e291febec625e0565d
1006820364@qq.com <1006820364@qq.com>
2019-08-30 11:05:36 +0800

    添加用户

---
+++ src/main/java/com/imooc/model/User.java
@@ -0,0 +1,21 @@
+package com.imooc.model;
+
+/**
+ * @program: config
+ * @description: 用户
+ * @author: liangpeng
+ * @create: 2019-08-30 11:04
+ **/
+public class User {
+
+    private String age;
+
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
